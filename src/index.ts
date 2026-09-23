760e3e2695c988512427132b57319da151e23381
Henry Sjøen <henry.sjoen@miles.no>
2026-09-23 12:49:30 +0200

    init cli project

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program.name("klaus-code").description("The claude code killer").version("1.0.0");
+
+program
+  .command("greet")
+  .description("Greet a user")
+  .argument("<name>", "The name of the user to greet")
+  .option("-u, --uppercase", "Convert the greeting to uppercase")
+  .action((name: string, options: { uppercase?: boolean }) => {
+    let greeting = `Hello, ${name}!`;
+    if (options.uppercase) {
+      greeting = greeting.toUpperCase();
+    }
+    console.log(greeting);
+  })
+
+program.parse(process.argv);
